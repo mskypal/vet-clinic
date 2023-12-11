1cd346b4fec4604dfaa87d81591f025ea358872a
sumskysum <skripal1995@gmail.com>
2023-12-11 15:30:20 +0200

    REQ-02: Email validation

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -2,10 +2,14 @@ package main.java.com.magicvet;
 
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     static String PASSWORD = "Default";
     static Scanner SCANNER = new Scanner(System.in);
+
+    static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
     public static void main(String[] args) {
 
@@ -13,7 +17,18 @@ public class Main {
 
     }
     public static void run(){
+        if (auth()) {
+            
+            registerNewClient();
+            
+            
+        }
 
+
+
+    }
+
+    static boolean auth(){
         boolean accepted = false;
         for (int i = 0; i < 3; i++){
             System.out.println("Password: ");
@@ -27,6 +42,39 @@ public class Main {
         }
         System.out.println(accepted ? "Welcome to the Magic Vet!" : "Application has been blocked.");
 
+        return accepted;
 
+    }
+    static void registerNewClient() {
+        System.out.println("Please, provide client details.");
+        System.out.print("Email: ");
+        String email = SCANNER.nextLine();
+
+        if (isEmailValid(email)){
+            Client client = buildClient(email);
+            System.out.println("New client: " + client.firstName + " " + client.lastName + " (" + client.email + ")");
+
+        } else {
+            System.out.println("Provided email is invalid");
+        }
+
+    }
+
+    static Client buildClient(String email) {
+        Client client = new Client();
+        client.email = email;
+
+        System.out.print("First name: ");
+        client.firstName = SCANNER.nextLine();
+
+        System.out.print("Last name: ");
+        client.lastName = SCANNER.nextLine();
+        return client;
+    }
+
+    static boolean isEmailValid(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
